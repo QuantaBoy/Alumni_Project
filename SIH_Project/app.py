9f8f497abece397b8a15da752b774dcebeb69853
QuantaBoy <quantachanger@gmail.com>
2025-11-19 20:21:16 +0530

    Small Changes

--- SIH_Project/app.py
+++ SIH_Project/app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template('base.html')
 
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
 @app.route('/login')
 def login():
     return render_template('login.html')
